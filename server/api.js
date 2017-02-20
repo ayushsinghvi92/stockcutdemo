5c95c5ade1945fba48578e197e69628a11cc31af
mosane <ayush.singhvi@nyu.edu>
2017-02-19 19:27:36 -0500

    updated bones

--- server/api.js
+++ server/api.js
@@ -7,12 +7,6 @@ api
   .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
-  .use('/calculate', require('./calculate'))
-
-// Send along any errors
-api.use((err, req, res, next) => {
-  res.status(500).send(err)
-})
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
--- server/rectangleData.js
+++
@@ -1,5 +0,0 @@
-const bins = require('../../node/binPackTest.js');
-
-const api = module.exports = require('express').Router()
-
-api.use((req,res) => res.send(bins))
